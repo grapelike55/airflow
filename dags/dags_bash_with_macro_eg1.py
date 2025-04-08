4542c741216337f23c14d31f04fbdb47c0373ec7
chanhpark <grapelike55@naver.com>
2025-04-09 08:03:46 +0900

    macro up

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -14,7 +14,7 @@ with DAG(
         task_id = 'bash_task_1',
         env={'START_DATE': '{{data_interval_start.in_timezone("Asia/Seoul") | ds}}', 
              'END_DATE' : '{{ data_intevel_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1) | ds}}'
-        }
+        },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )
     
