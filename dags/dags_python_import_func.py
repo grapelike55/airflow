e80d9052b5ae85558181f98922d207a9af0551f3
chanhpark <grapelike55@naver.com>
2025-04-09 01:27:50 +0900

    common func added

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -1,3 +1,4 @@
+
 from airflow.models.dag import DAG
 import datetime
 from airflow.operators.python import PythonOperator
@@ -7,19 +8,16 @@ import pendulum
 import random
 
 with DAG(
-    dag_id="dags_python_operator",
+    dag_id="dags_python_import_func",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-    def select_fruit():
-        fruit = ['APPLE','BANANA', 'ORANGE', 'AVOCADO']
-        rand_int = random.randint(0,3)
-        print(fruit[rand_int])
+
         
-    py_t1 = PythonOperator(
-        task_id='py_t1',
-        python_callable=select_fruit
+    task_get_sftp = PythonOperator(
+        task_id='task_get_sftp',
+        python_callable=get_sftp
     )
     
-    py_t1
+    task_get_sftp
