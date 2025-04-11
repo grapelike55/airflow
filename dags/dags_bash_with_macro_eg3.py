1cd032fc194102db66493b2a6055ac540b7dedf3
chanhpark <grapelike55@naver.com>
2025-04-11 14:57:43 +0900

    up

---
+++ dags/dags_bash_with_macro_eg3.py
@@ -0,0 +1,21 @@
+
+from airflow.models.dag import DAG
+import datetime
+from airflow.operators.bash import BashOperator
+
+import pendulum
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg3",
+    schedule="10 0 * * 6#2",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    bash_task_1 = BashOperator(
+        task_id = 'bash_task_1',
+        env={'START_DATE': '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19))}}', 
+             'END_DATE' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14))}}'
+        },
+        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
+    )
+    
