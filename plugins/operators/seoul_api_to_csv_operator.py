53a7c98741b020d4aeb69591209375738ae19dd4
chpark <grapelike55@naver.com>
2025-04-16 15:55:32 +0900

    up

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -36,7 +36,7 @@ class SeoulApiToCsvOperator(BaseOperator):
                 
         if not os.path.exists(self.path):
             os.system(f'mkdir -p {self.path}')
-        total_row_df.to_csv(self.path + '/' + self.file_name, enconding='utf-8', index=False)
+        total_row_df.to_csv(self.path + '/' + self.file_name, index=False)
         
     def _call_api(self, base_url, start_row, end_row):
         import requests
