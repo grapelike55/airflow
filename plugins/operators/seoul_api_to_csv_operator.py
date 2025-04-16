87414a8de36c33bf20bf9aa044e29731ceb841d4
chpark <grapelike55@naver.com>
2025-04-16 15:46:55 +0900

    up

---
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -0,0 +1,64 @@
+from airflow.models.baseoperator import BaseOperator
+from airflow.hooks.base import BaseHook
+import pandas as pd
+
+class SeoulApiToCsvOperator(BaseOperator):
+    template_fields=('endpoint', 'path', 'file_name', 'base_dt')
+    
+    def __init__(self, dataset_nm, path, file_name, base_dt=None, **kwargs):
+        super().__init__(**kwargs)
+        self.http_conn_id = 'data.seoul.go.kr'
+        self.path = path
+        self.file_name = file_name
+        self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/'+ dataset_nm
+        self.base_dt = base_dt
+        
+    def execute(self, context):
+        import os
+        connection = BaseHook.get_connection(self.http_conn_id)
+        self.base_url = f'http://{connection.host}:{connect.port}/{self.endpoint}'
+        print(self.base_url )
+        
+        total_row_df = pd.DataFrame()
+        start_row = 1
+        end_row = 1000
+        
+        while True:
+            self.log.info(f'시작:{start_row}')
+            self.log.info(f'끝:{end_row}')
+            row_df = self._call_api(self.base_url, start_row, end_row)
+            tatal_row_df = pd.concat([total_row_df, row_df])
+            if len(row_df) < 1000:
+                break
+            else:
+                start_row = end_row +1
+                end_row += 1000
+                
+        if not os.path.exists(self.path):
+            os.system(f'mkdir -p {self.path}')
+        total_row_df.to_csv(self.path + '/' + self.file_name, enconding='utf-8', index=false)
+        
+    def _call_api(self, base_url, start_row, end_row):
+        import requests
+        import json
+        
+        headers={'Content-Type':'application/json',
+                 'charset':'utf-8',
+                 'Accept': '*/*'
+        }
+        
+        request_url = f'{base_url}/{start_row}/{end_row}/'
+        if self.base_dt is not None:
+            request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
+        response = requests.get(request_url, headers)
+        contents = json.loads(response.text)
+        
+        key_nm = list(contents.keys())[0]
+        rown_data = contents.get(key_nm).get('row')
+        row_df = pd.DataFrame(row_data)
+        
+        return row_df
+            
+        
+        
+    
