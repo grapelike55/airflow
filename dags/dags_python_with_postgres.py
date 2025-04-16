a3f01f8100972f485a93836a3ae41052cac2c34e
chpark <grapelike55@naver.com>
2025-04-16 19:19:32 +0900

    up

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -27,7 +27,7 @@ with DAG(
                 cursor.execute(sql, (dag_id, task_id, run_id, msg))
                 conn.commit()
      
-     insrt_postgres = PythonOperator(
+        insrt_postgres = PythonOperator(
          task_id='insrt_postgres',
          python_callable=insrt_postgres,
          op_args=['172.28.0.3', '5432','chanhpark', 'chanhpark', 'chanhpark']
