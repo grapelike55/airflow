1e1f23ed50be544fac489a8f9becc04da4762bf3
chpark <grapelike55@naver.com>
2025-04-16 11:07:31 +0900

    up

---
+++ dags/dags_trigger_dag_run_operator.py
@@ -0,0 +1,42 @@
+from airflow.models.dag import DAG
+import datetime
+from airflow.operators.bash import BashOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+import pendulum
+
+with DAG(
+    dag_id="dags_trigger_dag_run_operator",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    
+    start_task = BashOperator(
+        task_id='start_task',
+        bash_command='echo "start!"',
+    )
+    
+    trigger_dag_task = TriggerDagRunOperator(
+        task_id='trigger_dag_task',
+        trigger_dag_id='dags_python_operator',
+        trigger_run_id=None,
+        execution_date='{{date_interval_start}}',
+        reset_dag_run=True,
+        wait_for_completion=False,
+        poke_interval=60,
+        allowed_states=['success'],
+        failed_states=None
+    )
+    
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+    
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+    
+    bash_t1 >> bash_t2
